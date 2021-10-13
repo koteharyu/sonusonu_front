546056104e42a1972976aa99878fe553bb7c43a2
haryu <hryau6@gmail.com>
2021-10-13 11:26:43 +0900

    add: routing of gorunals

---
+++ src/components/NewGournal.tsx
@@ -0,0 +1,7 @@
+import { memo, VFC } from "react";
+
+export const NewGournal: VFC = memo(() => {
+  return (
+    <p>New Grounal</p>
+  )
+})
--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,9 +1,9 @@
 import { memo, VFC } from "react";
 import { Switch, Route } from "react-router-dom";
-import { Gournal } from "../components/Gournal";
 import { Home } from "../components/Home";
 import { Menu } from "../components/Menu";
 import { Page404 } from "../components/Page404";
+import { GournalRoutes } from "./GournalRoutes";
 
 export const Router: VFC = memo(() => {
   return (
@@ -14,8 +14,15 @@ export const Router: VFC = memo(() => {
       <Route exact path="/menu">
         <Menu />
       </Route>
-      <Route exact path="/gournal">
-        <Gournal />
+      <Route path="/gournal" render={({ match: {url}}) => (
+        <Switch>
+          {GournalRoutes.map((route) => (
+            <Route key={route.path} exact={route.exact} path={`${url}${route.path}`}>
+              {route.children}
+            </Route>
+          ))}
+        </Switch>
+      )}>
       </Route>
       <Route path="*">
         <Page404 />
